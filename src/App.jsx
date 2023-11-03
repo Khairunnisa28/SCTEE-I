746f26317387b98a5823b5f3b3b311fca0190b78
Khairunnisa28 <138001140+Khairunnisa28@users.noreply.github.com>
2023-11-03 17:47:40 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,21 @@
-import React, { useState } from 'react';
-import Form from './component/Form';
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './component/navbar';
 import './App.css'
 
-function Greeting(props){
-  return(
-    <h2>Hello my name is {props.name}</h2>
-  )
-}
-
 function App() {
-  const [hitung, setHitung] = useState(0);
-
-  const tambahState = () =>{
-    setHitung(hitung+1)
-  }
-  const kurangState = () =>{
-    if (hitung > 0){
-      setHitung(hitung-1)
-    } else{
-      alert("Pengurangan State Telah Mencapai Batas")
-    }
-  }
   return (
-    <div className='App'>
-      <Greeting name="Nida"/>
-      <Form/>
-      <h1>Hitung State</h1>
-      <h3>Nilai State Sekarang: {hitung}</h3>
-      <p>*nilai state tidak boleh minus</p>
-      <button onClick={tambahState}>Tambah State</button>
-      <button onClick={kurangState}>Kurang State</button>
-    </div>
+    <BrowserRouter>
+      <Navbar/>
+      <div className='App'>
+        <div className='content'>
+          <Routes>
+            <Route path='/'/>
+            <Route path='/cart'/>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/component/navbar.jsx
@@ -0,0 +1,24 @@
+import Logo from '../image/Logo.png';
+
+function Navbar(){
+    return(
+        <nav className='navbar'>
+            <div className='logo'>
+                <img src={Logo} alt='Logo'/>
+            </div>
+            <ul className='nav-list'>
+                <li className='nav-item'>
+                    <a href="/">Home</a>
+                </li>
+                <li className='nav-item'>
+                    <a href="/Product">Product</a>
+                </li>
+                <li className='nav-item'>
+                    <a href="/contact">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
