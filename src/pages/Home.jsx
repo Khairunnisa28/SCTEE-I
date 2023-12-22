0a88deaea91f8f919a1ad1ee0b72901035551ae5
Khairunnisa28 <138001140+Khairunnisa28@users.noreply.github.com>
2023-12-22 17:15:51 +0700

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import Headling from '../image/head.jpg';
 import Cart_img from '../image/cart.png';
 import Ramen from "../image/ramen.jpeg";
+import Gyoza from "../image/gyoza.jpg";
+import './Home.css';
+import { useState } from 'react';
 
 function Home (){
+    const initialProducts = [{id:1, name:'Ramen', price: 45000, quantity: 0, image: Ramen},
+                            {id:2, name:'Gyoza', price: 25000, quantity: 0, image: Gyoza},
+                            {id:3, name:'Ramen', price: 45000, quantity: 0, image: Ramen},
+                            ]
+    const [products, setProducts] = useState(initialProducts);
+
     return(
         <div className="Content">    
             <img src={Headling} alt="Headling"/>
@@ -10,18 +19,24 @@ function Home (){
             <button>
                 <img src={Cart_img} alt="Keranjang Belanja"/>
             </button>
-            <div className='cards'>
-                <div className="card" style={{width: '18rem'}}>
-                    <img src={Ramen} className="card-img-top" alt="Ramen"/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
+            <div className='cards-container'>
+                {products.map((product) => (
+                    <div className="card" key={product.id} style={{width: '18rem'}}>
+                        <img src={product.image} className="card-img-top" alt="Ramen"/>
+                        <div className="card-body">
+                            <h5 className="card-title">{product.name}</h5>
+                            <p className="card-text">Rp. {product.price}</p>
+                            <a href="#" className="btn btn-primary">Order</a>
+                        </div>
+                    </div>                    
+                ))}
+
             </div>
         </div>
     )
 }
 
 export default Home;
+
+
+
